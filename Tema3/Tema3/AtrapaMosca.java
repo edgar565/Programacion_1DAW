5880ce6b9f53671fd6e0d801b903516417482b9d
edgsannic <edgarsancheznicolau@gmail.com>
2023-11-23 13:55:56 +0100

    Modificación de AtrapaMosca

--- Tema3/Tema3/AtrapaMosca.java
+++ Tema3/Tema3/AtrapaMosca.java
@@ -5,31 +5,50 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class AtrapaMosca {
+    static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
+    static
+
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
         int posicionMosca = random.nextInt(1, 15);
         int[] mosca = new int[15];
         mosca[posicionMosca] = 1;
         System.out.println(Arrays.toString(mosca));
         System.out.println(posicionMosca);
         System.out.println("Dime una posición entre 1 y 15");
+        juego(posicionMosca, mosca,);
+    }
+    public static int posicionJugador(){
         int posicion = scanner.nextInt();
         while (posicion < 1 || posicion > 15){
             System.out.println("Incorrecto, dime una posición entre 1 y 15");
             posicion = scanner.nextInt();
         }
-        if (posicionMosca == posicion - 1){
-            System.out.println(
-                    "  _   _    _    ____       _  _____ ____      _    ____   _    ____   ___    _        _      __  __  ___  ____   ____    _    \n" +
-                    " | | | |  / \\  / ___|     / \\|_   _|  _ \\    / \\  |  _ \\ / \\  |  _ \\ / _ \\  | |      / \\    |  \\/  |/ _ \\/ ___| / ___|  / \\   \n" +
-                    " | |_| | / _ \\ \\___ \\    / _ \\ | | | |_) |  / _ \\ | |_) / _ \\ | | | | | | | | |     / _ \\   | |\\/| | | | \\___ \\| |     / _ \\  \n" +
-                    " |  _  |/ ___ \\ ___) |  / ___ \\| | |  _ <  / ___ \\|  __/ ___ \\| |_| | |_| | | |___ / ___ \\  | |  | | |_| |___) | |___ / ___ \\ \n" +
-                    " |_| |_/_/   \\_\\____/  /_/   \\_\\_| |_| \\_\\/_/   \\_\\_| /_/   \\_\\____/ \\___/  |_____/_/   \\_\\ |_|  |_|\\___/|____/ \\____/_/   \\_\\\n");
-        } else {
-            while ((posicion - 2) || (posicion + 1))
-
+    }
+    public static void juego(int posicionMosca, int[] mosca, int posicion){
+        boolean victoria = false;
+        while (!victoria) {
+            posicionJugador();
+            if (posicionMosca == posicion){
+                victoria = true;
+                ascii();
+            } else if (posicion == posicionMosca - 1 || posicion == posicionMosca + 1) {
+                mosca[posicionMosca] = 0;
+                posicionMosca = random.nextInt(1, 15);
+                mosca[posicionMosca] = 1;
+                System.out.println("Estás cerca");
+            } else {
+                System.out.println("Esta muy lejos");
             }
         }
+    }
+    public static void ascii() {
+        System.out.println(
+                        "  _   _    _    ____       _  _____ ____      _    ____   _    ____   ___    _        _      __  __  ___  ____   ____    _    \n" +
+                        " | | | |  / \\  / ___|     / \\|_   _|  _ \\    / \\  |  _ \\ / \\  |  _ \\ / _ \\  | |      / \\    |  \\/  |/ _ \\/ ___| / ___|  / \\   \n" +
+                        " | |_| | / _ \\ \\___ \\    / _ \\ | | | |_) |  / _ \\ | |_) / _ \\ | | | | | | | | |     / _ \\   | |\\/| | | | \\___ \\| |     / _ \\  \n" +
+                        " |  _  |/ ___ \\ ___) |  / ___ \\| | |  _ <  / ___ \\|  __/ ___ \\| |_| | |_| | | |___ / ___ \\  | |  | | |_| |___) | |___ / ___ \\ \n" +
+                        " |_| |_/_/   \\_\\____/  /_/   \\_\\_| |_| \\_\\/_/   \\_\\_| /_/   \\_\\____/ \\___/  |_____/_/   \\_\\ |_|  |_|\\___/|____/ \\____/_/   \\_\\\n");
+
     }
 }
