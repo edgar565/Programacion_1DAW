f2e0e295605f85a44784ab6fe36b4c41eed9d821
edgar565 <edgarsancheznicolau@gmail.com>
2023-11-29 18:29:12 +0100

    Modificació de BuscarParejas i creació de Buscaminas

---
+++ Tema3/Tema3/Buscaminas.java
@@ -0,0 +1,37 @@
+package Tema3;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
+public class Buscaminas {
+    public static void main(String[] args) {
+        Random random = new Random();
+        String[] tablero = new String[]{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"};
+        for (int i = 1; i < 6; i++){
+            int x = random.nextInt(0,19);
+            tablero[x] = "*";
+            if (Objects.equals(tablero[x], "*")){
+                x = random.nextInt(0, 19);
+                tablero[x] = "*";
+            }
+        }
+        for (int i = 1; i < 19 ;i++){
+            if (tablero[i].equals("*")){
+                tablero[i - 1] = "1";
+                tablero[i + 1] = "1";
+            }
+        }
+        for (int i = 2; i < 17; i++){
+            if (tablero[i].equals("*") && tablero[i - 2].equals("*")){
+                tablero[i - 1] = "2";
+            } else if (tablero[i].equals("*") && tablero[i + 2].equals("*") ){
+                tablero[i + 1] = "2";
+            }
+        }
+        if (tablero[0].equals("*") && tablero[19].equals("*")){
+            tablero[1] = "1";
+            tablero[18] = "1";
+        }
+        System.out.println(Arrays.toString(tablero));
+    }
+}
--- Tema3/Tema3/BuscarParejas.java
+++ Tema3/Tema3/BuscarParejas.java
@@ -7,55 +7,60 @@ import java.util.Scanner;
 public class BuscarParejas {
     static Random random = new Random();
     static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args){
-        String[] animales = new String[]{"león", "mandril", "elefante", "suricata", "pingüino", "facóquero", "jirafa", "tigre", "koala", "panda", "elefante", "mandril", "jirafa", "facóquero", "pingüino","suricata", "tigre", "panda", "koala","león"};
+    public static void main(String[] args) {
+        String[] animales = new String[]{"león", "mandril", "elefante", "suricata", "pingüino", "facóquero", "jirafa", "tigre", "koala", "panda", "elefante", "mandril", "jirafa", "facóquero", "pingüino", "suricata", "tigre", "panda", "koala", "león"};
         String[] cartas = new String[20];
         Arrays.fill(cartas, "oculto");
         System.out.println(Arrays.toString(cartas));
-        for (int i = 0; i < 100; i++){
-            int aleatorio1 = random.nextInt(0,19);
-            int aleatorio2 = random.nextInt(0,19);
-            String x = animales[aleatorio1];
-            animales[aleatorio1] = animales[aleatorio2];
-            animales[aleatorio2] = x;
+        juego(cartas,animales);
+        System.out.println("HAS GANADO FELICIDADES!!");
+        System.out.println("Quieres volver a empezar? Si o no");
+        String seguir = scanner.toString();
+        while (seguir.equals("Si, si, SI")){
+            rotación(animales);
+            juego(cartas,animales);
         }
-        System.out.println(Arrays.toString(animales));
-        /*while () {
+    }
+    public static void juego(String[] cartas, String[] animales){
+        while (cartasMostradas(cartas) == false) {
             System.out.println("Dime la primera posición para desvelar entre 0 - 19");
             int posicion1 = scanner.nextInt();
-            if (posicion1 < 1 || posicion1 > 20) {
+            if (posicion1 < 0 || posicion1 > 19) {
                 System.out.println("Dime la primera posición para desvelar entre 0 - 19");
                 posicion1 = scanner.nextInt();
             }
             System.out.println("Dime la segunda posición para desvelar entre 0 - 19");
             int posicion2 = scanner.nextInt();
-            if (posicion2 < 1 || posicion2 > 20) {
+            if (posicion2 < 0 || posicion2 > 19 || posicion1 == posicion2) {
                 System.out.println("Dime la segunda posición para desvelar entre 0 - 19");
                 posicion2 = scanner.nextInt();
             }
-            System.out.println("La carta en la posición " + posicion1 + " es : ");
-            System.out.println("La carta en la posición " + posicion2 + " es : ");
-        }*/
+            System.out.println("La carta en la posición " + posicion1 + " es : " + animales[posicion1]);
+            System.out.println("La carta en la posición " + posicion2 + " es : " + animales[posicion2]);
+            if (animales[posicion1].equals(animales[posicion2])) {
+                cartas[posicion1] = animales[posicion1];
+                cartas[posicion2] = animales[posicion2];
+            }
+            System.out.println(Arrays.toString(cartas));
+        }
     }
 
-
-
-
-
-}
-/*for (int i = random.nextInt(0, 19), x = 0, c = 0; c < cartas.length; c++){
-        cartas[i] = animales[random.nextInt(0,9)];
-        while ( i != x){
-        cartas[i] = animales[random.nextInt(0,9)];
+    public static boolean cartasMostradas (String[] cartas){
+        boolean result = true;
+        for (int i = 0; i < 20; i++){
+            if (cartas[i].equals("oculto")){
+                result = false;
+            }
         }
-        }*/
-// tengo todas las cartes en cartas ordenadas
-// while 100
-//      aleatorio1
-//      aleatorio2
-//      intercambio
-
-// for 0 19 para cada carta
-//      // calculo aleatorio 0-9
-//      // comprobar que ixe aleatori está 1 o 0 vegades en cartes
-//      // VUelvo a calcular aleatorio
+        return result;
+    }
+    public static void rotación(String[] animales){
+        for (int i = 0; i < 100; i++) {
+            int aleatorio1 = random.nextInt(0, 19);
+            int aleatorio2 = random.nextInt(0, 19);
+            String x = animales[aleatorio1];
+            animales[aleatorio1] = animales[aleatorio2];
+            animales[aleatorio2] = x;
+        }
+    }
+}
