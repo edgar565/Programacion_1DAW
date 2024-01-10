681886dd4751b39e8c9a081376afd8b974c1b947
edgar565 <edgarsancheznicolau@gmail.com>
2024-01-10 19:15:15 +0100

    Modificació MyString

--- Tema3/Tema3/MyString.java
+++ Tema3/Tema3/MyString.java
@@ -3,13 +3,13 @@ package Tema3;
 import java.util.Scanner;
 
 public class MyString {
-    public static void main(String []args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("a. Función para invertir y poner en mayúsculas una cadena\n" +
                 "b. Función para devolver una cadena en vocales\n" +
                 "");
         String menu = scanner.next().toUpperCase();
-        switch (menu){
+        switch (menu) {
             case "A":
                 System.out.println("Dame la cadena");
                 String cadena1 = scanner.next();
@@ -18,27 +18,54 @@ public class MyString {
             case "B":
                 System.out.println("Dame la cadena");
                 String cadena2 = scanner.next();
-                System.out.println(vocales(cadena2));
+                System.out.println(contarVocales(cadena2));
                 break;
             case "C":
-
-
-
+                System.out.println("Dame la cadena");
+                String cadena3 = scanner.next();
+                System.out.println(mayorLongitud(cadena3));
+                break;
+            case "D":
+            case "E":
+                System.out.println("Dame la cadena");
+                String cadena4 = scanner.next();
+                //System.out.println(contarPalabras(cadena4));
+                break;
 
         }
     }
-    public static String invertirMayusculas(String cadena1){
+
+    public static String invertirMayusculas(String cadena) {
         String cadenaInvertida = "";
-        for (int x = cadena1.length() - 1; x >= 0; x--){
-            cadenaInvertida = cadenaInvertida + cadena1.charAt(x);
+        for (int x = cadena.length() - 1; x >= 0; x--) {
+            cadenaInvertida = cadenaInvertida + cadena.charAt(x);
         }
         return cadenaInvertida.toUpperCase();
     }
-    public static int vocales(String cadena2){
+
+    public static int contarVocales(String cadena) {
         int vocales = 0;
-        cadena2.toUpperCase();
-        for (int x = 0; x < cadena2.length(); x++){
+        cadena = cadena.toUpperCase();
+        for (int x = 0; x < cadena.length(); x++) {
+            if (cadena.charAt(x) == 'A' || cadena.charAt(x) == 'E' || cadena.charAt(x) == 'I' ||cadena.charAt(x) == 'O' || cadena.charAt(x) == 'U') {
+                vocales++;
+            }
         }
         return vocales;
     }
+    public static String mayorLongitud(String cadena) {
+        String[] palabra = cadena.split(" ");
+        String palabraMasGrande = "";
+        for (String palabra : palabra) {
+            if (palabra.length() > palabraMasGrande.length()) {
+                palabraMasGrande = palabra;
+            }
+        }
+        return palabraMasGrande;
+    }
+
+    /*public static int contarPalabras(String cadena) {
+        int palabras = cadena.indexOf("");;
+        //return cadena.repeat("");
+    }*/
 }
