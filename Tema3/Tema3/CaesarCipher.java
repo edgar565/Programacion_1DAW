1c50ede89820f01cf0f5c0e6513c266f7325df8f
edgsannic <edgarsancheznicolau@gmail.com>
2024-01-11 10:49:37 +0100

    .

---
+++ Tema3/Tema3/CaesarCipher.java
@@ -0,0 +1,30 @@
+package Tema3;
+
+import java.util.Scanner;
+
+public class CaesarCipher {
+    public static void main (String []args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("a. Cifrar mensaje\n" +
+                "b. Descifrar mensaje");
+        String menu = scanner.next().toUpperCase();
+        switch (menu){
+            case "A":
+
+                break;
+            case "B":
+
+                break;
+        }
+
+    }
+    public static String encrypt(){
+
+    }
+    public static String decrypt(){
+
+
+    }
+
+
+}
--- Tema3/Tema3/MyString.java
+++ Tema3/Tema3/MyString.java
@@ -3,34 +3,50 @@ package Tema3;
 import java.util.Scanner;
 
 public class MyString {
+   static String cadena;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("a. Función para invertir y poner en mayúsculas una cadena\n" +
                 "b. Función para devolver una cadena en vocales\n" +
-                "");
-        String menu = scanner.next().toUpperCase();
+                "c. Función para devolver la palabra de mayor longitud\n" +
+                "d. Función para devolver el número de veces que la segunda cadena está incluida\n" +
+                "e. Función para devolver el numero de palabras que contiene\n" +
+                "f. Función para devolver el número de teléfono en formato\n" +
+                "g. Procedimiento para que se muestre el histograma de vocales");
+        String menu = scanner.nextLine().toUpperCase();
+        System.out.println("Dame la cadena");
         switch (menu) {
             case "A":
-                System.out.println("Dame la cadena");
-                String cadena1 = scanner.next();
-                System.out.println(invertirMayusculas(cadena1));
+                cadena = scanner.nextLine();
+                System.out.println(invertirMayusculas(cadena));
                 break;
             case "B":
-                System.out.println("Dame la cadena");
-                String cadena2 = scanner.next();
-                System.out.println(contarVocales(cadena2));
+                 cadena = scanner.nextLine();
+                System.out.println(contarVocales(cadena));
                 break;
             case "C":
-                System.out.println("Dame la cadena");
-                String cadena3 = scanner.next();
-                System.out.println(mayorLongitud(cadena3));
+                cadena = scanner.nextLine();
+                System.out.println(mayorLongitud(cadena));
                 break;
             case "D":
-            case "E":
-                System.out.println("Dame la cadena");
-                String cadena4 = scanner.next();
-                //System.out.println(contarPalabras(cadena4));
+                cadena = scanner.nextLine();
+                System.out.println("Dime otra cadena");
+                String cadena2 = scanner.nextLine();
+                System.out.println(numeroVecesIncluida(cadena,cadena2));
                 break;
+            case "E":
+                cadena = scanner.nextLine();
+                System.out.println(contarPalabras(cadena));
+                break;
+            case "F":
+                cadena = scanner.nextLine();
+                System.out.println(numeroTel(cadena));
+                break;
+            case "G":
+                cadena = scanner.nextLine();
+                histograma(cadena);
+                break;
+
 
         }
     }
@@ -42,7 +58,6 @@ public class MyString {
         }
         return cadenaInvertida.toUpperCase();
     }
-
     public static int contarVocales(String cadena) {
         int vocales = 0;
         cadena = cadena.toUpperCase();
@@ -56,16 +71,65 @@ public class MyString {
     public static String mayorLongitud(String cadena) {
         String[] palabra = cadena.split(" ");
         String palabraMasGrande = "";
-        for (String palabra : palabra) {
-            if (palabra.length() > palabraMasGrande.length()) {
-                palabraMasGrande = palabra;
+        for (String x : palabra) {
+            if (x.length() > palabraMasGrande.length()) {
+                palabraMasGrande = x;
             }
         }
         return palabraMasGrande;
     }
-
-    /*public static int contarPalabras(String cadena) {
-        int palabras = cadena.indexOf("");;
-        //return cadena.repeat("");
-    }*/
+    public static int numeroVecesIncluida(String cadena, String cadena2){
+        int veces = 0;
+        for (int x = 0; x < cadena.length(); x++) {
+            if (cadena.startsWith(cadena2,x)){
+                veces++;
+            }
+        }
+        return veces;
+    }
+    public static int contarPalabras(String cadena) {
+        cadena.trim();
+        int palabras = 0;
+        for (int x = 0; x < cadena.length(); x++) {
+            if (cadena.charAt(x) == ' ') {
+                palabras++;
+            }
+        }
+        return palabras + 1;
+    }
+    public static String numeroTel(String cadena){
+        if (cadena.length() == 11){
+            cadena = String.format("(+%s)-%s-%s", cadena.substring(0,2), cadena.substring(3,6), cadena.substring(7,11));
+        }else {
+            cadena = "Número incorrecto";
+        }
+        return cadena;
+    }
+    public static void histograma(String cadena){
+        int a = 0;
+        int e = 0;
+        int i = 0;
+        int o = 0;
+        int u = 0;
+        cadena = cadena.toUpperCase();
+        for (int x = 0; x < cadena.length(); x++) {
+            if (cadena.charAt(x) == 'A') {
+                a++;
+            }if (cadena.charAt(x) == 'E') {
+                e++;
+            }if (cadena.charAt(x) == 'I') {
+                i++;
+            }if (cadena.charAt(x) == 'O') {
+                o++;
+            }if (cadena.charAt(x) == 'U') {
+                u++;
+            }
+        }
+        System.out.println("Repeticiones de vocales:\n" +
+                "a " + a + "\n" +
+                "e " + e + "\n" +
+                "i " + i + "\n" +
+                "o " + o + "\n" +
+                "u " + u);
+    }
 }
