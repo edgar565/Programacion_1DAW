082a8edf00e7b8f24c7b472a4c4f9331bc6cb8c9
edgar565 <edgarsancheznicolau@gmail.com>
2024-01-26 16:35:45 +0100

    .

--- Tema4/Tema4/CuentaBancaria.java
+++ Tema4/Tema4/CuentaBancaria.java
@@ -1,8 +1,71 @@
 package Tema4;
+import java.util.ArrayList;
 
 public class CuentaBancaria {
-        public String numeroCuenta;
-        public String titular;
-        public double saldo;
+    private final String iban;
+    private String titular;
+    private String dni;
+    private double saldo;
+    private final ArrayList<Double> movimientos;
+    public CuentaBancaria() {
+        iban = "ES0000000000000000000000";
+        titular = "LibreCoders";
+        dni = "000000000A";
+        movimientos = null;
 
+    }
+    public CuentaBancaria(String iban, String titular, String dni) {
+        this.iban = iban;
+        this.titular = titular;
+        this.dni = dni;
+        this.saldo = 0;
+        this.movimientos = new ArrayList<>();
+    }
+    public String getIban() {
+        return iban;
+    }
+    public String getTitular() {
+        return titular;
+    }
+    public String getDni() {
+        return dni;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public ArrayList<Double> getMovimientos() {
+        return movimientos;
+    }
+    public void realizarIngreso(double cantidad) {
+        if (cantidad > 0) {
+            saldo += cantidad;
+            movimientos.add(cantidad);
+            if (saldo < -50) {
+                System.out.println("AVISO: Saldo negativo");
+            }
+            if (cantidad > 3000) {
+                System.out.println("AVISO: Notificar a hacienda");
+            }
+        } else {
+            System.out.println("Error: La cantidad a ingresar debe ser mayor que cero.");
+        }
+    }
+    public void realizarRetirada(double cantidad) {
+        if (cantidad > 0) {
+            if (saldo - cantidad >= -50) {
+                saldo -= cantidad;
+                movimientos.add(-cantidad);
+                if (saldo < -50) {
+                    System.out.println("AVISO: Saldo negativo");
+                }
+                if (cantidad > 3000) {
+                    System.out.println("AVISO: Notificar a hacienda");
+                }
+            } else {
+                System.out.println("Error: Saldo insuficiente para realizar la retirada.");
+            }
+        } else {
+            System.out.println("Error: La cantidad a retirar debe ser mayor que cero.");
+        }
+    }
 }
--- Tema4/Tema4/DawBank.java
+++ Tema4/Tema4/DawBank.java
@@ -1,5 +1,135 @@
 package Tema4;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class DawBank {
-    String
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+        int opcionCliente;
+        CuentaBancaria cuenta = new CuentaBancaria();
+        do {
+            System.out.println("Menú Principal:");
+            System.out.println("1. Hacerte cliente");
+            System.out.println("2. Acceder cliente");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+            if (opcion == 1) {
+                System.out.println("\nPorfavor rellene los siguientes datos para crear la cuenta:\n");
+                System.out.print("Ingrese el titular de la cuenta: ");
+                String titular = scanner.nextLine();
+                System.out.print("Ingrese el dni del titular de la cuenta:");
+                String dni = scanner.nextLine();
+                System.out.println(" ### PORFAVOR ESPERE ###");
+                String iban = numeroCuenta();
+                cuenta = new CuentaBancaria(iban, titular,dni);
+                System.out.println("La cuenta ha sido creada correctamente");
+            }
+            if (opcion !=55) {
+                System.out.print("\nIngrese el titular de la cuenta: ");
+                String titular = scanner.nextLine();
+                System.out.print("Ingrese el dni del titular de la cuenta:");
+                String dni = scanner.nextLine();
+                System.out.println("\n ### PORFAVOR ESPERE ###");
+                String iban = numeroCuenta();
+                cuenta = new CuentaBancaria(iban, titular,dni);
+                do {
+                    System.out.println("\nMenú Principal:");
+                    System.out.println("1. Datos de la cuenta");
+                    System.out.println("2. IBAN");
+                    System.out.println("3. Titular");
+                    System.out.println("4. Saldo");
+                    System.out.println("5. Ingreso");
+                    System.out.println("6. Retirada");
+                    System.out.println("7. Movimientos");
+                    System.out.println("8. Salir");
+                    System.out.print("Seleccione una opción: ");
+                    opcionCliente = scanner.nextInt();
+                    switch (opcionCliente) {
+                        case 1:
+                            mostrarDatosCuenta(cuenta);
+                            break;
+                        case 2:
+                            mostrarIban(cuenta);
+                            break;
+                        case 3:
+                            mostrarTitular(cuenta);
+                            break;
+                        case 4:
+                            mostrarSaldo(cuenta);
+                            break;
+                        case 5:
+                            realizarIngreso(cuenta);
+                            break;
+                        case 6:
+                            realizarRetirada(cuenta);
+                            break;
+                        case 7:
+                            mostrarMovimientos(cuenta);
+                            break;
+                        case 8:
+                            System.out.println("¡Hasta luego!\n");
+                            break;
+                        default:
+                            System.out.println("Error: Opción no válida. Intente nuevamente.\n");
+                    }
+                } while (opcionCliente != 8);
+            }
+        } while (opcion != 55);
+    }
+
+    private static void mostrarDatosCuenta(CuentaBancaria cuenta) {
+        System.out.println("\n ### DATOS DE LA CUENTA ###");
+        System.out.println("IBAN: " + cuenta.getIban());
+        System.out.println("Titular: " + cuenta.getTitular());
+        System.out.println("DNI: " + cuenta.getDni());
+        System.out.println("Saldo: " + cuenta.getSaldo() + " euros");
+        System.out.println();
+    }
+    public static void mostrarIban(CuentaBancaria cuenta) {
+        System.out.println("\nIBAN: " + cuenta.getIban());
+        System.out.println();
+    }
+    public static void mostrarTitular(CuentaBancaria cuenta) {
+        System.out.println("\nTITULAR: " + cuenta.getTitular());
+        System.out.println();
+    }
+    public static void mostrarSaldo(CuentaBancaria cuenta) {
+        System.out.println("\nSALDO: " + cuenta.getSaldo() + " euros");
+        System.out.println();
+    }
+    public static void realizarIngreso(CuentaBancaria cuenta) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nINGRESE LA CANTIDAD A INGRESAR: ");
+        double cantidad = scanner.nextDouble();
+        cuenta.realizarIngreso(cantidad);
+        System.out.println();
+    }
+    public static void realizarRetirada(CuentaBancaria cuenta) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nINGRESE LA CANTIDAD A RETIRAR: ");
+        double cantidad = scanner.nextDouble();
+        cuenta.realizarRetirada(cantidad);
+        System.out.println();
+    }
+    public static void mostrarMovimientos(CuentaBancaria cuenta) {
+        System.out.println("\nMOVIMIENTOS:");
+        ArrayList<Double> movimientos = cuenta.getMovimientos();
+        for (int i = 0; i < movimientos.size(); i++) {
+            System.out.println(i + 1 + ". " + (movimientos.get(i) > 0 ? "Ingreso" : "Retirada") + ": " +
+                    Math.abs(movimientos.get(i)) + " euros");
+        }
+        System.out.println();
+    }
+    public static String numeroCuenta() {
+        StringBuilder numero = new StringBuilder();
+        Random random = new Random();
+        for (int x = 0; x < 22; x++) {
+            numero.append(random.nextInt(10));
+        }
+        return "ES" + numero;
+    }
 }
